bf2004e1055e45c6660e641f1d34f9fcdc69c156
Tomek RasPi <lukom.tomek@gmail.com>
2023-05-06 22:15:05 +0200

    Make site look prettier and usable on light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start space-y-10 bg-white dark:bg-zinc-900 dark:text-white">
-      <div className="flex items-center justify-between h-16 w-full bg-zinc-950 p-5">
+    <main className="flex min-h-screen flex-col items-center justify-start space-y-12 px-[30%] bg-white dark:bg-zinc-900 dark:text-white text-justify">
+      <div className="fixed top-0 flex items-center justify-between h-16 w-full bg-gray-100 dark:bg-zinc-950 p-5 backdrop-blur-[2px] navbar">
         <h1 className="text-lg">Ant Studios</h1>
         <div className="w-1/4 flex items-center justify-between menu">
           <span>About</span>
