2a966486fad27903b74cd298d54b07a838b3af8e
mineTomek <lukom.tomek@gmail.com>
2023-05-06 19:46:01 +0200

    Add cool page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from 'next/image'
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-start space-y-10 bg-white dark:bg-zinc-900 dark:text-white">
+      <div className="flex items-center justify-between h-16 w-full bg-zinc-950 p-5">
+        <h1 className="text-lg">Ant Studios</h1>
+        <div className="w-1/4 flex items-center justify-between menu">
+          <span>About</span>
+          <span>Our Games</span>
+          <span>Contact</span>
+        </div>
+      </div>
+
+      {/* <div className="bg-red-500 w-3/6 h-48 rounded-lg hover:w-2/6 resize hover:skew-x-6 transition-all ease-in-out duration-50">
+
+      </div> */}
     </main>
   )
 }
