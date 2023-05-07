e28b501b0f317af903bea48b3943fa1579b6212d
Tomek RasPi <lukom.tomek@gmail.com>
2023-05-08 01:05:31 +0200

    Make the menu items links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
       <div className="fixed top-0 flex items-center justify-between h-16 w-full bg-white/50 dark:bg-zinc-900/50 p-5 backdrop-blur-[2px] border-b border-gray-200 navbar">
         <h1 className="text-xl self-center w-1/2">Ant Studios</h1>
         <div className="w-1/2 justify-end space-x-5 hidden sm:flex menu">
-          <span>About</span>
-          <span>Our Games</span>
-          <span>Contact</span>
+          <a href="#">About</a>
+          <a href="#">Our Games</a>
+          <a href="#">Contact</a>
         </div>
         <div className="w-1/2 justify-end space-x-5 flex sm:hidden menu">
           <FontAwesomeIcon icon={faBars} className="w-12 h-12"/>
