ad97016d950eda23584c6037e4d4faa9e7301e5f
mineTomek <lukom.tomek@gmail.com>
2023-05-08 16:45:44 +0200

    Fix white being visible when scrolling too far

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " bg-white dark:bg-zinc-900"}>{children}</body>
     </html>
   )
 }
