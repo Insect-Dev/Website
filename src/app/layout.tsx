fe6bed8c71cdcb5dd6d5853b7b0b193453c10f1a
mineTomek <lukom.tomek@gmail.com>
2023-05-10 19:39:42 +0200

    Update title to match new organization name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Ant Studios',
+  title: 'Anthrodev',
   description: 'Official Anthrodev website',
 }
 
