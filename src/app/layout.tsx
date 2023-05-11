34d4f4cbf2b05a618c2d7d7e302d4bae07c557df
mineTomek <lukom.tomek@gmail.com>
2023-05-11 16:51:58 +0200

    Update all important files using Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Anthrodev',
-  description: 'Official Anthrodev website',
-}
+    title: "Anthrodev",
+    description: "Official Anthrodev website",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className + " bg-white dark:bg-zinc-900"}>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className + " bg-white dark:bg-zinc-900"}>
+                {children}
+                <Analytics />
+            </body>
+        </html>
+    );
 }
