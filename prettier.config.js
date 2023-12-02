63459b604a9d3f85ccbade20ca1652daefc1fab9
mineTomek <lukom.tomek@gmail.com>
2023-12-02 15:42:36 +0100

    Format code

--- prettier.config.js
+++ prettier.config.js
@@ -4,5 +4,5 @@ module.exports = {
   jsxSingleQuote: true,
   semi: false,
   singleQuote: true,
-  plugins: ['prettier-plugin-tailwindcss']
+  plugins: ['prettier-plugin-tailwindcss'],
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-      Insect Dev
-    </main>
-  )
+  return <main>Insect Dev</main>
 }
