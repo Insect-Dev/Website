999326dad26df099efe52199e536ab645d7b2cbe
mineTomek <lukom.tomek@gmail.com>
2023-12-11 16:07:39 +0100

    Add GameDemo component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
-export default function Navbar() {
+export default function Navbar(props: {hide: boolean}) {
   return (
-    <div className='sticky top-0 flex justify-end'>
+    <div className={`sticky top-0 justify-end ${props.hide ? 'hidden' : 'flex'}`}>
       <div className='flex h-16 w-1/2 items-center justify-end gap-8 bg-gradient-to-l from-gray-800 to-transparent px-8'>
         <span>Home</span>
         <span>About Us</span>
---
+++ src/app/components/home/GameDemo.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { faMaximize, faMinimize } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { MutableRefObject, useState } from 'react'
+import { Unity, useUnityContext } from 'react-unity-webgl'
+
+export default function GameDemo(props: { onGameFullscreen: (newFullscreen: boolean) => void }) {
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    dataUrl: 'scalar/Scalar.data',
+    frameworkUrl: 'scalar/Scalar.framework.js',
+    loaderUrl: 'scalar/Scalar.loader.js',
+    codeUrl: 'scalar/Scalar.wasm',
+  })
+
+  const [fullscreen, setFullscreen] = useState(false)
+
+  const changeFullscreen = (callback: (previous: boolean) => boolean) => {
+    const newFullscreen = callback(fullscreen)
+    props.onGameFullscreen(newFullscreen)
+    setFullscreen(newFullscreen)
+  }
+
+  return (
+    <div
+      className={`group flex items-center justify-center ${
+        fullscreen
+          ? 'fixed bottom-0 left-0 right-0 top-0'
+          : 'relative aspect-video w-full rounded-lg border'
+      }`}
+    >
+      {!isLoaded && (
+        <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+      )}
+      <Unity
+        unityProvider={unityProvider}
+        className={`${isLoaded ? 'block' : 'hidden'}  h-full w-full rounded-lg`}
+      />
+      <div className='absolute bottom-2 right-2 flex h-12 w-12 cursor-pointer items-center justify-center rounded-md opacity-10 transition-opacity duration-500 [background-image:radial-gradient(circle_closest-side,#0003,#fff0)] group-hover:opacity-100'>
+        <FontAwesomeIcon
+          icon={fullscreen ? faMinimize : faMaximize}
+          className='h-8 w-8'
+          onClick={() => changeFullscreen(prev => !prev)}
+        />
+      </div>
+    </div>
+  )
+}
